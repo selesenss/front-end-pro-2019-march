501675d35a9a73b4baebc41e862b34aa19bb65bf
savelii.sychov <savelii.sychov@myheritage.com>
2019-04-04 19:39:47 +0300

    L_7

---
+++ lections/lection_7_objects/materials/main.js
@@ -0,0 +1,61 @@
+console.log('Lection 7');
+
+
+var data = {
+    x: 10,
+    info: {
+        adress: 'Mechnikova',
+        city: 'Kiev'
+    }
+}
+
+data.z = false;
+
+// console.log(data.value, data.x);
+
+
+
+
+// -----------------------------------------
+
+var some = 'val'
+
+data.val = 150;
+data['val'] = 250;
+// console.log(data, data['val']);
+
+// ----------------------------------------
+
+for(var key in data){
+    console.log(key, '------------', data[key]);
+
+    if (key == 'x') {
+        console.log('XXX')
+        data.some = true;
+        data.val = undefined;
+        // delete data.val;
+    }
+}
+
+// var list = [123,23,123,123]
+// for(var i = 0; i < list.length; i++){
+//     console.log(list[i])
+// }
+
+
+// ----------------------------------------
+
+
+var info = {
+    y: 10,
+    sum: function(a, b){
+        return a + b;
+    }
+}
+
+info.sum(20, 10);
+Math.random()
+console.log()
+document.write();
+
+console.log(Math)
