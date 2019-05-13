a6b94bb7c81cf24e5185e3d67566eb447313cbec
savelii.sychov <savelii.sychov@myheritage.com>
2019-05-13 21:00:38 +0300

    L_14 upd

--- lections/lection_14_DOM/materials/events.js
+++ lections/lection_14_DOM/materials/events.js
@@ -5,7 +5,7 @@ window.onload = function () {
         console.log(ev.type, ev)
     }
 
-    block.addEventListener('click', handler);
+    // block.addEventListener('click', handler);
     // block.addEventListener('mouseover', handler);
     // block.addEventListener('mouseout', handler);
     // block.addEventListener('mousemove', handler);
@@ -13,5 +13,29 @@ window.onload = function () {
     // block.addEventListener('mousedown', handler);
     // block.addEventListener('mouseup', handler);
     // block.addEventListener('contextmenu', handler);
+
+    var x, y, diffX, diffY, a, b, startX, startY;
+
+    function onMouseDown(event) {
+        x = event.clientX;
+        y = event.clientY;
+        startX = event.target.offsetLeft;
+        startY = event.target.offsetTop;
+
+        document.addEventListener('mousemove', onMouseMove)
+    }
+
+    function onMouseMove(event) {
+        block.style.left = startX + event.clientX - x + 'px'
+        block.style.top = startY + event.clientY - y + 'px'
+    }
+
+    function onMouseUp(event) {
+        document.removeEventListener('mousemove', onMouseMove)
+    }
+
+    block.addEventListener('mousedown', onMouseDown)
+
+    block.addEventListener('mouseup', onMouseUp)
 
 }
