e5e2c9192913499826dd97eec3e8607cb67569e8
savelii.sychov <savelii.sychov@myheritage.com>
2019-06-06 20:33:35 +0300

    L_21

--- lections/lection_21_Promises/materials/main.js
+++ lections/lection_21_Promises/materials/main.js
@@ -23,49 +23,49 @@ var example = new Promise(function(resolve, reject){
 })
 
 
-example
-    .then(
-        function(value){
-            console.log('Resolved', value);
-            // return value * 20;
-            return Promise.resolve(value * 20);
-        }, 
-        function(value){
-            console.warn('Rejected', value);
+// example
+//     .then(
+//         function(value){
+//             console.log('Resolved', value);
+//             // return value * 20;
+//             return Promise.resolve(value * 20);
+//         }, 
+//         function(value){
+//             console.warn('Rejected', value);
 
-            // return Promise.resolve();
-            return Promise.reject(value);
-        }
-    )
-    .then(function(value){
-        console.log('Resolved step 2', value);
-    }, function(value){
-        console.warn('Rejected step 2', value);
-    })
-    .catch();
+//             // return Promise.resolve();
+//             return Promise.reject(value);
+//         }
+//     )
+//     .then(function(value){
+//         console.log('Resolved step 2', value);
+//     }, function(value){
+//         console.warn('Rejected step 2', value);
+//     })
+//     .catch();
 
-//----------------------------
-function getRandom(from, to){
-    return Math.floor(Math.random()*(to - from) + from);
-}
+// //----------------------------
+// function getRandom(from, to){
+//     return Math.floor(Math.random()*(to - from) + from);
+// }
 
-    new Promise(function(resolve, reject){
-        setTimeout(function(){
-            var list = [];
-            list.length = getRandom(0, 10)
-            for(var i = 0; i < list.length; i++){
-                list[i] = getRandom(-20, 20)
-            }
-            resolve(list);
-        }, 500)
-    })
-    .then(onSuccess)
-    .then(filterData)
-    .then(saveToStorage)
-    .catch(function(error){ console.error(error) })
-    .finally(function(){
-        console.log(arguments)
-    })
+//     new Promise(function(resolve, reject){
+//         setTimeout(function(){
+//             var list = [];
+//             list.length = getRandom(0, 10)
+//             for(var i = 0; i < list.length; i++){
+//                 list[i] = getRandom(-20, 20)
+//             }
+//             resolve(list);
+//         }, 500)
+//     })
+//     .then(onSuccess)
+//     .then(filterData)
+//     .then(saveToStorage)
+//     .catch(function(error){ console.error(error) })
+//     .finally(function(){
+//         console.log(arguments)
+//     })
 
 function onSuccess(list){
     // code
@@ -95,3 +95,60 @@ function saveToStorage(list){
 // ---------------------------------------------
 
 // Promise.all()
+
+
+function asyncExampleOne(resolve, reject){
+    setTimeout(function(){
+        resolve('3s')
+    }, 3000)
+}
+
+function asyncExampleTwo(resolve, reject){
+    setTimeout(function(){
+        resolve('5s')
+    }, 5000)
+}
+
+function asyncExampleThree(resolve, reject){
+    setTimeout(function(){
+        reject('1s')
+    }, 6000)
+}
+
+
+// new Promise(asyncExampleOne)
+//     .then(function(value){
+//         console.log(value);
+//         return new Promise(asyncExampleTwo);
+//     })
+//     .then(function(value){
+//         console.log(value);
+//         return new Promise(asyncExampleTwo);
+//     })
+//     .then(function(value){
+//         console.log(value);
+//         return new Promise(asyncExampleOne);
+//     })
+//     .then(function(value){
+//         console.log(value);
+//     })
+
+
+var listOfAsync = [
+    new Promise(asyncExampleOne), 
+    new Promise(asyncExampleTwo),
+    25,
+    Promise.reject(),
+    new Promise(asyncExampleOne),
+    // new Promise(asyncExampleThree)
+];
+
+Promise
+    .all(listOfAsync)
+    .then(function(values){
+        console.log(values);
+    })
+    .catch(function(err){
+        console.error(err);
+    });
+
