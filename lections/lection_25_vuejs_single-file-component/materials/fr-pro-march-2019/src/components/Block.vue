86dd34d3d8015470b9aebc5fe4551601711b1f5e
savelii.sychov <savelii.sychov@myheritage.com>
2019-07-02 07:22:51 +0300

    25

---
+++ lections/lection_25_vuejs_single-file-component/materials/fr-pro-march-2019/src/components/Block.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="block" :style="{width: width + 'px'}">
+        {{title}} {{width}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'block',
+    props: ['title', 'width']
+}
+</script>
+
+<style>
+    .block {
+        width: 300px;
+        height: 20px;
+        background-color: aquamarine;
+    }
+</style>
---
+++ lections/lection_25_vuejs_single-file-component/materials/fr-pro-march-2019/src/components/ExampleModel.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <hr>
+        <!-- <input type="text" @input="title=$event.target.value" :value="title"> -->
+        <input type="text" @input="onChange" :value="title">
+        <input type="text" v-model="title">
+        title: {{title}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'example',
+    data(){
+        return {
+            title: 'Defa'
+        }
+    },
+    methods: {
+        onChange(ev){
+            this.title = ev.target.value;
+            // console.log(ev)
+            if(!isNaN(+ev.data)){
+                var val = this.title.split('')
+                val.pop();
+                this.title = val.join('');
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
